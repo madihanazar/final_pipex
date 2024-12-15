19aa39619fa0f6c12b62118ff69e0c5ba2a35e49
madihanazar <143177070+madihanazar@users.noreply.github.com>
2024-12-15 21:37:32 +0400

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnazar <mnazar@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 17:51:25 by mnazar            #+#    #+#             */
+/*   Updated: 2024/12/15 15:22:19 by mnazar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen_gnl(const char *c)
+{
+	int	i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup_gnl(const char *s)
+{
+	char	*ans;
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	ans = (char *)malloc(i + 1 * sizeof(char));
+	if (ans == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ans[i] = s[i];
+		i++;
+	}
+	ans[i] = '\0';
+	return (ans);
+}
+
+char	*ft_strjoin_gnl(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	res = (char *)malloc((ft_strlen_gnl(s1) + ft_strlen_gnl(s2) + 1)
+			* sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	res[i + j] = '\0';
+	return (res);
+}
+
+char	*ft_strchr_gnl(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+		{
+			return ((char *)(str + i));
+		}
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)(str + i));
+	return (NULL);
+}
+
+char	*ft_substr_gnl(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	sub = (char *)malloc(len + 1 * sizeof(char));
+	if (sub == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
